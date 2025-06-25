9d07f9268977377fabe2b4e98ac0a17de0f57db9
Chand Mohammad <145117735+AdvanceBoyWeb@users.noreply.github.com>
2025-06-26 03:33:54 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get(`${import.meta.env.VITE_API_URL}/book`);
+        const res = await axios.get("https://book-store-emxf.vercel.app/);
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
