80267795eaacd43e3da19e66e1279639498134f5
Chand Mohammad <145117735+AdvanceBoyWeb@users.noreply.github.com>
2025-06-27 16:45:11 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -86,7 +86,7 @@ function Navbar() {
               </div>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 dark:bg-slate-800 dark:text-white rounded-box w-52"
               >
                 {navItems}
               </ul>
