b2fd083177bc2cf49351313e485a706eebe2b3eb
Chand Mohammad <145117735+AdvanceBoyWeb@users.noreply.github.com>
2025-06-28 15:52:05 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -23,7 +23,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post(`${import.meta.env.VITE_API_URL}/user/login`, userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
